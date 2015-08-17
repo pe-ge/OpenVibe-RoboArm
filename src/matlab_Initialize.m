f3cdf28cc48650284bed9a51a9cce32470390240
petopege <gergelp@gmail.com>
2015-08-17 18:28:25 +0200

    sending stimulations

--- src/matlab_Initialize.m
+++ src/matlab_Initialize.m
@@ -1,11 +1,27 @@
 function box_out = matlab_Initialize(box_in)
     disp('Matlab initialize function has been called.')
     
-    global nFFT sampleFreq freq_weights electrode_weights;
+    %%%% global variables used in process function
+    global nFFT sampleFreq freq_weights electrode_weights ref_average threshold threshold_window stimulation;
     nFFT = 256;
     sampleFreq = 128;
+    average = [];
+    threshold = 0.3;
+    threshold_window = [];
+    stimulation = hex2dec('00008003'); %%%% OVTK_StimulationId_SegmentStart
     load freq_weights;
     load electrode_weights;
+    
+    %%%% set output header
+    nb_channels = 1;
+    nb_samples_per_buffer = 1;
+    channel_names = cell(1);
+    channel_names{1} = sprintf('channel %i',1);
+    sampling_rate = 1;
+        
+    box_in = OV_setStimulationOutputHeader(box_in, 1);
+    box_in = OV_setSignalOutputHeader(box_in, 2, nb_channels, nb_samples_per_buffer, channel_names, sampling_rate);
+    
     
     box_out = box_in;
 end
--- src/matlab_Process.m
+++ src/matlab_Process.m
@@ -1,10 +1,11 @@
 function box_out = matlab_Process(box_in)
 
-	  global nFFT sampleFreq freq_weights electrode_weights;
+	  global nFFT sampleFreq freq_weights electrode_weights ref_average threshold threshold_window stimulation;
 
     for i = 1: OV_getNbPendingInputChunk(box_in,1)
 
         [box_in, start_time, end_time, matrix_data] = OV_popInputBuffer(box_in,1);
+        stim_set = [];
         
         %%%% iterate over all electrodes
         for i = 1 : size(matrix_data, 1)
@@ -30,23 +31,47 @@ function box_out = matlab_Process(box_in)
             spect = spect(fLines <= 25);
             fLines = fLines(fLines <= 25);
             
-            %%%% multiply spect with frequency and electrode weights
-            result(i) = (freq_weights' * spect) * electrode_weights(i); 
+            %%%% scale spect by multiplying with frequency weight and electrode weight
+            one_dim_signal(i) = (freq_weights' * spect) * electrode_weights(i); 
         end
         
-        result = sum(result);
+        %%%% final signal is sum of 
+        one_dim_signal = sum(one_dim_signal);
+
+        %%%% store processed signal for first 20s in order to compute reference average
+        if (end_time < 20)
+            ref_average(length(ref_average) + 1) = one_dim_signal;
+            
+        %%%% in next 10s compute averages for window of overlapping 4 signal values
+        elseif (end_time < 30)
+            %%%% compute reference average if not computed
+            if (length(ref_average) ~= 1)
+                ref_average = mean(ref_average);
+            end
+            
+            %%%% fill threshold_window with 4 signal values
+            if (length(threshold_window) ~= 4)
+                threshold_window(length(threshold_window) + 1) = one_dim_signal;
+            %%%% length = 4
+            else
+                %%%% compute average and compare with reference average
+                [ref_average mean(threshold_window)]
+                if (ref_average * (1 - threshold) > mean(threshold_window))
+                    %%%% if below reference average, send trigger to robotic arm
+                    stim_set = [stimulation; box_in.clock; 0];
+                    disp('Sending a stimulation...');
+                end
+                
+                %%%% move threshold window 
+                threshold_window(1:3) = threshold_window(2:4);
+                threshold_window(4) = one_dim_signal;
+            end
+        end
         
-        %%%% setting output header
-        box_in.outputs{1}.header = box_in.inputs{1}.header;
-        box_in.outputs{1}.header.nb_channels = 1;
-        channel_names = cell(1);
-        channel_names{1} = sprintf('channel %i',1);
-        box_in.outputs{1}.header.channel_names = channel_names;
-        box_in.outputs{1}.header.nb_samples_per_buffer = 1;
-        box_in.outputs{1}.header.sampling_rate = 1;
+        %%%% sending stimulation and signal
+        box_in = OV_addOutputBuffer(box_in,1,start_time,end_time,stim_set);
+        box_in = OV_addOutputBuffer(box_in,2,start_time,end_time,one_dim_signal);
         
-        %%%% sending output
-        box_in = OV_addOutputBuffer(box_in,1,start_time,end_time,result);
     end
 
     box_out = box_in;
